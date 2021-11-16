ba5bb5435c41fa4a39324bdcf0d7f69e6c8fcd53
Paul Seal <prjseal@gmail.com>
2021-11-16 22:55:01 +0000

    - got it working in v9 for first time

---
+++ CSUSK.Site/wwwroot/csusk-assets/scripts/navigationMenu.js
@@ -0,0 +1,18 @@
+﻿$('li.dropdown :first-child').not('.textonly').on('click', function () {
+    var $el = $(this).parent();
+    if ($el.hasClass('open')) {
+        var $a = $el.children('a.dropdown-toggle');
+        if ($a.length && $a.attr('href')) {
+            location.href = $a.attr('href');
+        }
+    }
+});
+
+$('.navbar-nav a[href^="' + location.pathname + '"]')
+    .each(function (i, element) {
+        var href = $(element).attr('href');
+        if (href == location.pathname || href == location.pathname + decodeURIComponent(location.search)) {
+            $(element).parent().addClass('active');
+        }
+    }
+);
