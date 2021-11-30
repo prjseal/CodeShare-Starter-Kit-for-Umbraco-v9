9c5be52b631d89342987bbca816eba64cc37856d
Paul Seal <prjseal@gmail.com>
2021-11-30 00:23:15 +0000

    - alpha 3 release

---
+++ CodeShare.Core/Components/ContactViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using CodeShare.Core.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeShare.Core.Components
+{
+    [ViewComponent(Name = "Contact")]
+    public class ContactViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ContactViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ CodeShare/Migrations/StarterKitPackageMigrationPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace CodeShare.Migrations
+{
+    public class StarterKitPackageMigrationPlan : PackageMigrationPlan
+    {
+        public StarterKitPackageMigrationPlan()
+            : base("CodeShare-Starter-Kit")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<ImportPackageXmlMigration>(new Guid("555ADD40-7569-451C-926D-AA89E7113966"));
+        }
+    }
+}
